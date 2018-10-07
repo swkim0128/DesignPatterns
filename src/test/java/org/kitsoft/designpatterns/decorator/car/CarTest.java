1cd83df8ddf2eee422869bbb0cf49748d4282457
swkim0128 <swkim0128@users.noreply.github.com>
2018-10-07 22:07:58 +0900

    revise package test designpatterns -add

---
+++ src/test/java/org/kitsoft/designpatterns/decorator/car/CarTest.java
@@ -0,0 +1,22 @@
+package org.kitsoft.designpatterns.decorator.car;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+class CarTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
