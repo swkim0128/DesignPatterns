011f903c73abf784164349b61de0082af8a64af3
swkim0128 <swkim0128@users.noreply.github.com>
2018-11-26 23:52:17 +0900

    combined pattern add
    mvc 패턴에서 자료에 있는 combined 예제 추가

---
+++ src/main/java/org/kitsoft/designpatterns/combined/djview/BPMObserver.java
@@ -0,0 +1,5 @@
+package org.kitsoft.designpatterns.combined.djview;
+  
+public interface BPMObserver {
+	void updateBPM();
+}
---
+++ src/main/java/org/kitsoft/designpatterns/combined/djview/BeatObserver.java
@@ -0,0 +1,5 @@
+package org.kitsoft.designpatterns.combined.djview;
+  
+public interface BeatObserver {
+	void updateBeat();
+}
